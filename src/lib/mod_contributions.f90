5832518335418c401e64fc1f73a44cc726333432
Ned Taylor <nedttaylor@gmail.com>
2024-03-19 10:37:45 +0000

    Rewrite evalulate_contribution procedure

--- src/lib/mod_contributions.f90
+++ src/lib/mod_contributions.f90
@@ -1,5 +1,6 @@
 module contributions
   use constants, only: real12
+  use misc, only: jump
 
 
 contains
@@ -7,92 +8,80 @@ contains
 !!!#############################################################################
 !!! evalulate the contribution of bondlength to the energy
 !!!#############################################################################
-subroutine evaluate_contribution (element_a,element_b,&
-  &bondlength_target,return_slot)
- character(*), intent(in) :: element_a, element_b
- real(real12), intent(in) :: bondlength_target
- real(real12), intent(out) :: return_slot
- character(1024) :: name 
- integer :: atom_number_a, atom_number_b, stat, i
- real(real12), dimension(3) :: value_read
- real(real12), dimension(3) :: bondlength_read  
- real(real12) :: a,b,c,d
- write(name,'(A,A,A,A,A)') "Devolved/",trim(adjustl(element_a))&
-    &,"_",trim(adjustl(element_b)),"_evolved_bondlength_gauss"
- open(199, file=trim(adjustl(name)))
- bondlength_read=0 
- value_read=0
- return_slot=0
- bondlength_read(2)=bondlength_target
+  subroutine evaluate_contribution (element_1, element_2, &
+       bondlength, amplitude)
+    implicit none
+    character(*), intent(in) :: element_1, element_2
+    real(real12), intent(in) :: bondlength
+    real(real12), intent(out) :: amplitude
+    
+    integer :: i, ierror, unit
+    real(real12) :: step_size, fraction
+    character(1024) :: filename
+    real(real12), dimension(2) :: amplitude_list = 0._real12
+    real(real12), dimension(2) :: bondlength_list = 0._real12
+
+    
+    !! Open the file containing evolved bondlengths
+    write(filename,'("Devolved/",A,"_",A,"_evolved_bondlength_gauss")') &
+         trim(adjustl(element_1)), trim(adjustl(element_2))
+    open(newunit=unit, file=trim(adjustl(filename)))
+    amplitude = 0._real12
+    
+    !! read the step size
+    read(unit,*,iostat=ierror) step_size
+    if(is_iostat_end(ierror)) then
+       close(unit); return
+    end if
+    call jump(unit, floor(bondlength/step_size) - 1)
+    do i = 1, 2
+       read(unit,*,iostat=ierror) bondlength_list(i), amplitude_list(i)
+       write(*,*) i, bondlength_list(i), amplitude_list(i)
+       if(is_iostat_end(ierror)) then
+          close(unit)
+          return
+       end if
+    end do
+
+    !!Assuming the line between points can be modelled as linear
+    fraction = (bondlength - bondlength_list(1)) / step_size
  
+    amplitude = amplitude_list(1) + &
+       (amplitude_list(2) - amplitude_list(1)) * fraction
+
+    !! THIS IS A HACK TO MAKE THE BOND LENGTH CONTRIBUTION DECAY
+    !!Arbitrary could be added for when bondlength contribution starts to decay,
+    !! could be changed to be bond specific (should be)!
+    if (bondlength.lt.1.6_real12) then 
+       amplitude = amplitude 
+    else if (bondlength.lt.3._real12) then  
+       amplitude = amplitude
+    else 
+       amplitude = amplitude / 26._real12
+    end if
  
- master :do while(1.eq.1)
- 
-  bondlength_read(1)=bondlength_read(3)
-  value_read(1)=value_read(3) 
- 
-  read(199,*,IOSTAT=stat) bondlength_read(3), value_read(3) 
- 
-  IF(IS_IOSTAT_END(stat)) then 
-     return_slot=0
-     close(199)
-     exit master
-  end IF
-  IF((bondlength_read(2).lt.bondlength_read(3)).and.&
-       &(bondlength_read(2).gt.bondlength_read(1))) then 
-     !!Assuming the line between points can be modelled as linear
-     a=bondlength_read(1)
-     b=bondlength_read(2) 
-     c=bondlength_read(3) 
- 
-     d=(b-a)/(c-a)
- 
- 
- 
-     if(value_read(3).lt.value_read(1)) d=1-d
- 
-     value_read(2)=value_read(1)+dble((value_read(3)-value_read(1))*d)
-     !!Arbitrary could be added for when bondlength contribution starts to decay,
-     !! could be changed to be bond specific (should be)!
- 
- 
-     return_slot=value_read(2)
-     if (bondlength_read(2).lt.1.6) then 
-        return_slot=return_slot 
-     else if (bondlength_read(2).lt.3.0) then  
- 
-        return_slot=return_slot
-     else 
-        return_slot=return_slot/26
-     end if
- 
-     close(199)
- 
-     exit master
-  end IF
- 
-  IF(IS_IOSTAT_END(stat)) exit
- end do master
- end subroutine evaluate_contribution
+    close(unit)
+
+  end subroutine evaluate_contribution
 !!!#############################################################################
  
  
 !!!#############################################################################
 !!! evalulate the contribution of bondangle to the energy
 !!!#############################################################################
- subroutine evaluate_angle_contribution (element_a,&
+ subroutine evaluate_angle_contribution (element_1,&
   &bondangle_target,return_slot)
- character(*) :: element_a
- character(1024) :: name
- integer :: atom_number_a, atom_number_b, stat, i
- real(real12), dimension(3) :: value_read
- real(real12), dimension(3) :: angles_read
+ character(*) :: element_1
+ character(1024) :: filename
+ integer :: stat, i
+ real(real12), dimension(3) :: value_read, angles_read
  real(real12) :: a,b,c,d, bondangle_target, return_slot
  
+ integer :: unit
  
- write(name,'(A,A,A,A,A)') "Devolved/",trim(adjustl(element_a))&
+ write(filename,'(A,A,A,A,A)') "Devolved/",trim(adjustl(element_1))&
     &,"_evolved_angles_gauss"
- open(199, file=trim(adjustl(name)))
+ open(newunit=unit, file=trim(adjustl(filename)))
  angles_read=0
  value_read=0
  angles_read(2)=bondangle_target
@@ -103,11 +92,11 @@ subroutine evaluate_contribution (element_a,element_b,&
   angles_read(1)=angles_read(3)
   value_read(1)=value_read(3)
  
-  read(199,*,IOSTAT=stat) angles_read(3), value_read(3)
+  read(unit,*,IOSTAT=stat) angles_read(3), value_read(3)
  
   IF(IS_IOSTAT_END(stat)) then
      return_slot=0
-     close(199)
+     close(unit)
      exit
   end IF
   IF((angles_read(2).lt.angles_read(3)).and.&
@@ -126,7 +115,7 @@ subroutine evaluate_contribution (element_a,element_b,&
  
  
      return_slot=value_read(2)
-     close(199)
+     close(unit)
      if(return_slot.gt.0) then
         !write(*,*) value_read(1), value_read(3), d,b, return_slot
      end if
@@ -144,35 +133,36 @@ subroutine evaluate_contribution (element_a,element_b,&
 !!!#############################################################################
 !!! evalulate the contribution of 4body to the energy
 !!!#############################################################################
- subroutine evaluate_4body_contribution (element_a,&
+ subroutine evaluate_4body_contribution (element_1,&
   &bondangle_target,return_slot)
- character(*) :: element_a
- character(1024) :: name
- integer :: atom_number_a, atom_number_b, stat, i
+ character(*) :: element_1
+ character(1024) :: filename
+ integer :: stat, i
  real(real12), dimension(3) :: value_read
  real(real12), dimension(3) :: angles_read
  real(real12) :: a,b,c,d, bondangle_target, return_slot
  
- 
- write(name,'(A,A,A,A,A)') "Devolved/",trim(adjustl(element_a))&
+ integer :: unit
+
+ write(filename,'(A,A,A,A,A)') "Devolved/",trim(adjustl(element_1))&
     &,"_evolved_4body_gauss"
- open(199, file=trim(adjustl(name)))
+ open(newunit=unit, file=trim(adjustl(filename)))
  angles_read=0
  value_read=0
  angles_read(2)=bondangle_target
  if(bondangle_target.eq.1000) then 
   return_slot=1
-  close(199)
+  close(unit)
  else
   master :do while(1.eq.1)
  
      angles_read(1)=angles_read(3)
      value_read(1)=value_read(3)
  
-     read(199,*,IOSTAT=stat) angles_read(3), value_read(3)
+     read(unit,*,IOSTAT=stat) angles_read(3), value_read(3)
      IF(IS_IOSTAT_END(stat)) then
         return_slot=0
-        close(199)
+        close(unit)
         exit
      end IF
      IF((angles_read(2).lt.angles_read(3)).and.&
@@ -202,7 +192,7 @@ subroutine evaluate_contribution (element_a,element_b,&
         if(return_slot.gt.0) then 
            !            write(*,*) value_read(1), value_read(3), d,b, return_slot
         end if
-        close(199)
+        close(unit)
         exit master
      end IF
      IF(IS_IOSTAT_END(stat)) exit

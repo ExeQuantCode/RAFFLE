b35e8c50782caa27c01f8cc40ee2034318fe3247
Ned Taylor <nedttaylor@gmail.com>
2024-03-21 08:32:28 +0000

    Convert to using bas_type

--- src/lib/mod_isolated.f90
+++ src/lib/mod_isolated.f90
@@ -1,5 +1,6 @@
 module isolated
   use constants, only: real12
+  use rw_geom, only: bas_type, geom_write
   use vasp_file_handler, only: generate_potcar, Jobwrite, Incarwrite
   implicit none
 
@@ -9,12 +10,17 @@ module isolated
 
 contains
 
+!!!#############################################################################
+!!! make isolated atom directories and set up enclosing calculations
+!!!#############################################################################
   subroutine generate_isolated_calculations(element_list)
     implicit none
     character(len=3), dimension(:), intent(in) :: element_list
 
     integer :: unit
     integer :: i, num_species
+    type(bas_type) :: basis
+    real(real12), dimension(3,3) :: lattice = 0._real12
     logical :: exists
     character(len=1024) :: tmp
 
@@ -29,15 +35,23 @@ contains
        call execute_command_line("mkdir iso") 
     end if
 
-
+    lattice(1,1) = 20._real12
+    lattice(2,2) = 20._real12
+    lattice(3,3) = 20._real12
+    basis%nspec = 1
+    basis%natom = 1
+    allocate(basis%spec(1))
+    basis%spec(1)%num = 1
+    allocate(basis%spec(1)%atom(1,3), source = 0.5_real12)
     !!--------------------------------------------------------------------------
     !!! prepare isolation calculations for each element
     !!--------------------------------------------------------------------------
     do i = 1, num_species 
+       basis%sysname = trim(adjustl(element_list(i)))//" isolated"
+       basis%spec(1)%name = trim(adjustl(element_list(i)))
 
-    
        !! write the name of the directory to tmp
-       write(tmp,'("iso/POSCAR_",A3)') element_list(i)
+       write(tmp,'("iso/POSCAR_",A3)') trim(adjustl(element_list(i)))
 
        !! check if directory already exists, if not, create it
        !! COMPLAIN IF EXISTS
@@ -50,31 +64,18 @@ contains
           !! make directory
           call execute_command_line("mkdir " // trim(adjustl(tmp)))
     
-          open(newunit=unit, &
-               file="iso/POSCAR_"//trim(adjustl(element_list(i)))//"/POSCAR") 
-          write(unit,'(A)') "test"
-          write(unit, *) "1.00000000"
-          write(unit, '(" 20.0000000000000000  0.0000000000000000  0.0000000000000000")')
-          write(unit, '("  0.0000000000000000 20.0000000000000000  0.0000000000000000")')
-          write(unit, '("  0.0000000000000000  0.0000000000000000 20.0000000000000000")')
-          write(unit,*) element_list(i) 
-          write(unit,*) 1
-          write(unit,'("Direct")')
-          write(unit,'("0.5000000000000000 0.5000000000000000 0.5000000000000000")')
+          !! write POSCAR file
+          open(newunit=unit, file=trim(adjustl(tmp))//"/POSCAR")
+          call geom_write(unit, lattice, basis)
           close(unit)
     
           call generate_potcar(tmp,[element_list(i)]) 
           call Jobwrite(tmp,1,1,1)
           call Incarwrite(tmp,500, 20) !!The 500 here is nstep electronic
-          !write(*,*) trim(adjustl(tmp))
-          call chdir(trim(adjustl(tmp)))
-          call execute_command_line("qsub.sh")
-          call execute_command_line("cp ../../job_vasp_isca.in .")
-          call chdir("../../")
-    
        end if
     end do
 
   end subroutine generate_isolated_calculations
+!!!#############################################################################
 
 end module isolated

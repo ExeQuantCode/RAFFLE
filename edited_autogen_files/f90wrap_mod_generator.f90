5c208d176aa6902eaa80e7a97cab1f6e05306f1d
Ned Taylor <nedttaylor@gmail.com>
2024-07-17 15:07:29 +0100

    Edit autogenerated wrapper files

---
+++ edited_autogen_files/f90wrap_mod_generator.f90
@@ -0,0 +1,374 @@
+! Module generator defined in file /Users/nedtaylor/DCoding/DGit/raffle/src/lib/mod_generator.f90
+
+subroutine f90wrap_stoichiometry_type__get__element(this, f90wrap_element)
+    use generator, only: stoichiometry_type
+    implicit none
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    integer, intent(in)   :: this(2)
+    type(stoichiometry_type_ptr_type) :: this_ptr
+    character(3), intent(out) :: f90wrap_element
+    
+    this_ptr = transfer(this, this_ptr)
+    f90wrap_element = this_ptr%p%element
+end subroutine f90wrap_stoichiometry_type__get__element
+
+subroutine f90wrap_stoichiometry_type__set__element(this, f90wrap_element)
+    use generator, only: stoichiometry_type
+    implicit none
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    integer, intent(in)   :: this(2)
+    type(stoichiometry_type_ptr_type) :: this_ptr
+    character(3), intent(in) :: f90wrap_element
+    
+    this_ptr = transfer(this, this_ptr)
+    this_ptr%p%element = f90wrap_element
+end subroutine f90wrap_stoichiometry_type__set__element
+
+subroutine f90wrap_stoichiometry_type__get__num(this, f90wrap_num)
+    use generator, only: stoichiometry_type
+    implicit none
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    integer, intent(in)   :: this(2)
+    type(stoichiometry_type_ptr_type) :: this_ptr
+    integer, intent(out) :: f90wrap_num
+    
+    this_ptr = transfer(this, this_ptr)
+    f90wrap_num = this_ptr%p%num
+end subroutine f90wrap_stoichiometry_type__get__num
+
+subroutine f90wrap_stoichiometry_type__set__num(this, f90wrap_num)
+    use generator, only: stoichiometry_type
+    implicit none
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    integer, intent(in)   :: this(2)
+    type(stoichiometry_type_ptr_type) :: this_ptr
+    integer, intent(in) :: f90wrap_num
+    
+    this_ptr = transfer(this, this_ptr)
+    this_ptr%p%num = f90wrap_num
+end subroutine f90wrap_stoichiometry_type__set__num
+
+subroutine f90wrap_stoichiometry_type_initialise(this)
+    use generator, only: stoichiometry_type
+    implicit none
+    
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    type(stoichiometry_type_ptr_type) :: this_ptr
+    integer, intent(out), dimension(2) :: this
+    allocate(this_ptr%p)
+    this = transfer(this_ptr, this)
+end subroutine f90wrap_stoichiometry_type_initialise
+
+subroutine f90wrap_stoichiometry_type_finalise(this)
+    use generator, only: stoichiometry_type
+    implicit none
+    
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    type(stoichiometry_type_ptr_type) :: this_ptr
+    integer, intent(in), dimension(2) :: this
+    this_ptr = transfer(this, this_ptr)
+    deallocate(this_ptr%p)
+end subroutine f90wrap_stoichiometry_type_finalise
+
+
+subroutine f90wrap_stoich_type_xnum_array__array_getitem__items( &
+       this, f90wrap_i, itemsitem)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    integer, intent(in), dimension(2) :: this
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(in) :: f90wrap_i
+    integer, intent(out) :: itemsitem(2)
+    type(stoichiometry_type_ptr_type) :: items_ptr
+    
+    this_ptr = transfer(this, this_ptr)
+    if (f90wrap_i < 1 .or. f90wrap_i > size(this_ptr%p%items)) then
+        call f90wrap_abort("array index out of range")
+    else
+        items_ptr%p => this_ptr%p%items(f90wrap_i)
+        itemsitem = transfer(items_ptr,itemsitem)
+    endif
+end subroutine f90wrap_stoich_type_xnum_array__array_getitem__items
+
+subroutine f90wrap_stoich_type_xnum_array__array_setitem__items(this, f90wrap_i, itemsitem)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type stoichiometry_type_ptr_type
+        type(stoichiometry_type), pointer :: p => NULL()
+    end type stoichiometry_type_ptr_type
+    integer, intent(in), dimension(2) :: this
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(in) :: f90wrap_i
+    integer, intent(out) :: itemsitem(2)
+    type(stoichiometry_type_ptr_type) :: items_ptr
+    
+    this_ptr = transfer(this, this_ptr)
+    if (f90wrap_i < 1 .or. f90wrap_i > size(this_ptr%p%items)) then
+        call f90wrap_abort("array index out of range")
+    else
+        items_ptr = transfer(itemsitem,items_ptr)
+        this_ptr%p%items(f90wrap_i) = items_ptr%p
+    endif
+end subroutine f90wrap_stoich_type_xnum_array__array_setitem__items
+
+subroutine f90wrap_stoich_type_xnum_array__array_len__items(this, f90wrap_n)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    integer, intent(in), dimension(2) :: this
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(out) :: f90wrap_n
+    this_ptr = transfer(this, this_ptr)
+    f90wrap_n = size(this_ptr%p%items)
+end subroutine f90wrap_stoich_type_xnum_array__array_len__items
+
+subroutine f90wrap_stoich_type_xnum_array__array_alloc__items(this, num)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(in) :: num
+    integer, intent(inout), dimension(2) :: this
+
+    this_ptr = transfer(this, this_ptr)
+    allocate(this_ptr%p%items(num))
+    this = transfer(this_ptr, this)
+end subroutine f90wrap_stoich_type_xnum_array__array_alloc__items
+
+subroutine f90wrap_stoich_type_xnum_array__array_dealloc__items(this)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(inout), dimension(2) :: this
+
+    this_ptr = transfer(this, this_ptr)
+    deallocate(this_ptr%p%items)
+    this = transfer(this_ptr, this)
+end subroutine f90wrap_stoich_type_xnum_array__array_dealloc__items
+
+
+subroutine f90wrap_generator__stoich_type_xnum_array_initialise(this)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(out), dimension(2) :: this
+    allocate(this_ptr%p)
+    this = transfer(this_ptr, this)
+end subroutine f90wrap_generator__stoich_type_xnum_array_initialise
+
+subroutine f90wrap_generator__stoich_type_xnum_array_finalise(this)
+    use generator, only: stoichiometry_type
+    implicit none
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type(stoichiometry_type_xnum_array_ptr_type) :: this_ptr
+    integer, intent(in), dimension(2) :: this
+    this_ptr = transfer(this, this_ptr)
+    deallocate(this_ptr%p)
+end subroutine f90wrap_generator__stoich_type_xnum_array_finalise
+
+
+
+
+subroutine f90wrap_raffle_generator_type__array__bins(this, nd, dtype, dshape, dloc)
+    use generator, only: raffle_generator_type
+    use, intrinsic :: iso_c_binding, only : c_int
+    implicit none
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+    integer(c_int), intent(in) :: this(2)
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer(c_int), intent(out) :: nd
+    integer(c_int), intent(out) :: dtype
+    integer(c_int), dimension(10), intent(out) :: dshape
+    integer*8, intent(out) :: dloc
+    
+    nd = 1
+    dtype = 5
+    this_ptr = transfer(this, this_ptr)
+    dshape(1:1) = shape(this_ptr%p%bins)
+    dloc = loc(this_ptr%p%bins)
+end subroutine f90wrap_raffle_generator_type__array__bins
+
+subroutine f90wrap_raffle_generator_type__array__lattice_host(this, nd, dtype, dshape, dloc)
+    use generator, only: raffle_generator_type
+    use, intrinsic :: iso_c_binding, only : c_int
+    implicit none
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+    integer(c_int), intent(in) :: this(2)
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer(c_int), intent(out) :: nd
+    integer(c_int), intent(out) :: dtype
+    integer(c_int), dimension(10), intent(out) :: dshape
+    integer*8, intent(out) :: dloc
+    
+    nd = 2
+    dtype = 11
+    this_ptr = transfer(this, this_ptr)
+    dshape(1:2) = shape(this_ptr%p%lattice_host)
+    dloc = loc(this_ptr%p%lattice_host)
+end subroutine f90wrap_raffle_generator_type__array__lattice_host
+
+subroutine f90wrap_raffle_generator_type__array__method_probab(this, nd, dtype, dshape, dloc)
+    use generator, only: raffle_generator_type
+    use, intrinsic :: iso_c_binding, only : c_int
+    implicit none
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+    integer(c_int), intent(in) :: this(2)
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer(c_int), intent(out) :: nd
+    integer(c_int), intent(out) :: dtype
+    integer(c_int), dimension(10), intent(out) :: dshape
+    integer*8, intent(out) :: dloc
+    
+    nd = 1
+    dtype = 11
+    this_ptr = transfer(this, this_ptr)
+    dshape(1:1) = shape(this_ptr%p%method_probab)
+    dloc = loc(this_ptr%p%method_probab)
+end subroutine f90wrap_raffle_generator_type__array__method_probab
+
+subroutine f90wrap_generator__raffle_generator_type_initialise(this)
+    use generator, only: raffle_generator_type
+    implicit none
+    
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer, intent(out), dimension(2) :: this
+    allocate(this_ptr%p)
+    this = transfer(this_ptr, this)
+end subroutine f90wrap_generator__raffle_generator_type_initialise
+
+subroutine f90wrap_generator__raffle_generator_type_finalise(this)
+    use generator, only: raffle_generator_type
+    implicit none
+    
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer, intent(in), dimension(2) :: this
+    this_ptr = transfer(this, this_ptr)
+    deallocate(this_ptr%p)
+end subroutine f90wrap_generator__raffle_generator_type_finalise
+
+subroutine f90wrap_generator__generate__binding__raffle_generator_type(this, num_structures, stoichiometry, &
+    method_probab, n0)
+    use generator, only: raffle_generator_type, stoichiometry_type
+    implicit none
+    
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+
+
+    type stoichiometry_type_xnum_array
+        type(stoichiometry_type), dimension(:), allocatable :: items
+    end type stoichiometry_type_xnum_array
+
+    type stoichiometry_type_xnum_array_ptr_type
+        type(stoichiometry_type_xnum_array), pointer :: p => NULL()
+    end type stoichiometry_type_xnum_array_ptr_type
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer, intent(in), dimension(2) :: this
+    integer, intent(in) :: num_structures
+    type(stoichiometry_type_xnum_array_ptr_type) :: stoichiometry_ptr
+    integer, intent(in), dimension(2) :: stoichiometry
+    real(4), intent(in), optional, dimension(n0) :: method_probab
+    integer :: n0
+    !f2py intent(hide), depend(method_probab) :: n0 = shape(method_probab,0)
+    this_ptr = transfer(this, this_ptr)
+    stoichiometry_ptr = transfer(stoichiometry, stoichiometry_ptr)
+    call this_ptr%p%generate(num_structures=num_structures, stoichiometry=stoichiometry_ptr%p%items, &
+        method_probab=method_probab)
+end subroutine f90wrap_generator__generate__binding__raffle_generator_type
+
+subroutine f90wrap_generator__print_hello__binding__raffle_generator_type(this)
+    use generator, only: raffle_generator_type
+    implicit none
+    
+    type raffle_generator_type_ptr_type
+        type(raffle_generator_type), pointer :: p => NULL()
+    end type raffle_generator_type_ptr_type
+    type(raffle_generator_type_ptr_type) :: this_ptr
+    integer, intent(in), dimension(2) :: this
+    this_ptr = transfer(this, this_ptr)
+    call this_ptr%p%print_hello()
+end subroutine f90wrap_generator__print_hello__binding__raffle_generator_type
+
+! End of module generator defined in file /Users/nedtaylor/DCoding/DGit/raffle/src/lib/mod_generator.f90
+
---
+++ edited_autogen_files/raffle.py
@@ -0,0 +1,369 @@
+from __future__ import print_function, absolute_import, division
+import _raffle
+import f90wrap.runtime
+import logging
+import numpy
+
+class Generator(f90wrap.runtime.FortranModule):
+    """
+    Module generator
+    
+    
+    Defined at ../src/lib/mod_generator.f90 lines \
+        1-286
+    
+    """
+    @f90wrap.runtime.register_class("raffle.stoichiometry_type")
+    class stoichiometry_type(f90wrap.runtime.FortranDerivedType):
+        """
+        Type(name=stoichiometry_type)
+        
+        
+        Defined at ../src/lib/mod_generator.f90 lines \
+            19-21
+        
+        """
+        def __init__(self, handle=None):
+            """
+            self = Stoichiometry_Type()
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                19-21
+            
+            
+            Returns
+            -------
+            this : Stoichiometry_Type
+            	Object to be constructed
+            
+            
+            Automatically generated constructor for stoichiometry_type
+            """
+            f90wrap.runtime.FortranDerivedType.__init__(self)
+            result = _raffle.f90wrap_generator__stoichiometry_type_initialise()
+            self._handle = result[0] if isinstance(result, tuple) else result
+        
+        def __del__(self):
+            """
+            Destructor for class Stoichiometry_Type
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                19-21
+            
+            Parameters
+            ----------
+            this : Stoichiometry_Type
+            	Object to be destructed
+            
+            
+            Automatically generated destructor for stoichiometry_type
+            """
+            if self._alloc:
+                _raffle.f90wrap_generator__stoichiometry_type_finalise(this=self._handle)
+        
+        @property
+        def element(self):
+            """
+            Element element ftype=character(len=3) pytype=str
+            
+            
+            Defined at ../src/lib/mod_generator.f90 line \
+                20
+            
+            """
+            return _raffle.f90wrap_stoichiometry_type__get__element(self._handle)
+        
+        @element.setter
+        def element(self, element):
+            _raffle.f90wrap_stoichiometry_type__set__element(self._handle, element)
+        
+        @property
+        def num(self):
+            """
+            Element num ftype=integer  pytype=int
+            
+            
+            Defined at ../src/lib/mod_generator.f90 line \
+                21
+            
+            """
+            return _raffle.f90wrap_stoichiometry_type__get__num(self._handle)
+        
+        @num.setter
+        def num(self, num):
+            _raffle.f90wrap_stoichiometry_type__set__num(self._handle, num)
+        
+        def __str__(self):
+            ret = ['<stoichiometry_type>{\n']
+            ret.append('    element : ')
+            ret.append(repr(self.element))
+            ret.append(',\n    num : ')
+            ret.append(repr(self.num))
+            ret.append('}')
+            return ''.join(ret)
+        
+        _dt_array_initialisers = []
+        
+    
+    @f90wrap.runtime.register_class("raffle.stoichiometry_type_xnum_array")
+    class stoichiometry_type_xnum_array(f90wrap.runtime.FortranDerivedType):
+        """
+        Type(name=stoichiometry_type_xnum_array)
+        
+        
+        Defined at ../src/lib/mod_generator.f90 lines \
+            19-21
+        
+        """
+        def __init__(self, handle=None):
+            """
+            self = Stoichiometry_Type()
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                19-21
+            
+            
+            Returns
+            -------
+            this : Stoichiometry_Type
+            	Object to be constructed
+            
+            
+            Automatically generated constructor for stoichiometry_type
+            """
+            f90wrap.runtime.FortranDerivedType.__init__(self)
+            result = _raffle.f90wrap_generator__stoich_type_xnum_array_initialise()
+            self._handle = result[0] if isinstance(result, tuple) else result
+        
+        def __del__(self):
+            """
+            Destructor for class Stoichiometry_Type
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                19-21
+            
+            Parameters
+            ----------
+            this : Stoichiometry_Type
+            	Object to be destructed
+            
+            
+            Automatically generated destructor for stoichiometry_type
+            """
+            if self._alloc:
+                _raffle.f90wrap_generator__stoich_type_xnum_array_finalise(this=self._handle)
+        
+        def init_array_items(self):
+            self.items = f90wrap.runtime.FortranDerivedTypeArray(self,
+                                            _raffle.f90wrap_stoich_type_xnum_array__array_getitem__items,
+                                            _raffle.f90wrap_stoich_type_xnum_array__array_setitem__items,
+                                            _raffle.f90wrap_stoich_type_xnum_array__array_len__items,
+                                            """
+            Element items ftype=type(test_type) pytype=Test_Type
+            
+            
+            Defined at  line 0
+            
+            """, Generator.stoichiometry_type)
+            return self.items
+        
+        def allocate(self, size):
+            """
+            Allocate the items array with the given size
+            
+            Parameters
+            ----------
+            self : Stoichiometry_Type
+            size : int
+                Size of the items array
+            """
+            _raffle.f90wrap_stoich_type_xnum_array__array_alloc__items(self._handle, num=size)
+
+        def deallocate(self):
+            """
+            Deallocate the items array
+            """
+            _raffle.f90wrap_stoich_type_xnum_array__array_dealloc__items(self._handle)
+
+        
+
+        _dt_array_initialisers = [init_array_items]
+        
+    
+    @f90wrap.runtime.register_class("raffle.raffle_generator_type")
+    class raffle_generator_type(f90wrap.runtime.FortranDerivedType):
+        """
+        Type(name=raffle_generator_type)
+        
+        
+        Defined at ../src/lib/mod_generator.f90 lines \
+            23-34
+        
+        """
+        def __init__(self, handle=None):
+            """
+            self = Raffle_Generator_Type()
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                23-34
+            
+            
+            Returns
+            -------
+            this : Raffle_Generator_Type
+            	Object to be constructed
+            
+            
+            Automatically generated constructor for raffle_generator_type
+            """
+            f90wrap.runtime.FortranDerivedType.__init__(self)
+            result = _raffle.f90wrap_generator__raffle_generator_type_initialise()
+            self._handle = result[0] if isinstance(result, tuple) else result
+        
+        def __del__(self):
+            """
+            Destructor for class Raffle_Generator_Type
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                23-34
+            
+            Parameters
+            ----------
+            this : Raffle_Generator_Type
+            	Object to be destructed
+            
+            
+            Automatically generated destructor for raffle_generator_type
+            """
+            if self._alloc:
+                _raffle.f90wrap_generator__raffle_generator_type_finalise(this=self._handle)
+        
+        def print_hello(self):
+            """
+            print_hello__binding__raffle_generator_type(self)
+            
+            
+            Defined at ../src/lib/mod_generator.f90 lines \
+                69-74
+            
+            Parameters
+            ----------
+            this : unknown
+            
+            """
+            _raffle.f90wrap_generator__print_hello__binding__raffle_generator_type(this=self._handle)
+        
+        @property
+        def bins(self):
+            """
+            Element bins ftype=integer pytype=int
+            
+            
+            Defined at ../src/lib/mod_generator.f90 line \
+                24
+            
+            """
+            array_ndim, array_type, array_shape, array_handle = \
+                _raffle.f90wrap_raffle_generator_type__array__bins(self._handle)
+            if array_handle in self._arrays:
+                bins = self._arrays[array_handle]
+            else:
+                bins = f90wrap.runtime.get_array(f90wrap.runtime.sizeof_fortran_t,
+                                        self._handle,
+                                        _raffle.f90wrap_raffle_generator_type__array__bins)
+                self._arrays[array_handle] = bins
+            return bins
+        
+        @bins.setter
+        def bins(self, bins):
+            self.bins[...] = bins
+        
+        @property
+        def lattice_host(self):
+            """
+            Element lattice_host ftype=real(real12) pytype=float
+            
+            
+            Defined at ../src/lib/mod_generator.f90 line \
+                25
+            
+            """
+            array_ndim, array_type, array_shape, array_handle = \
+                _raffle.f90wrap_raffle_generator_type__array__lattice_host(self._handle)
+            if array_handle in self._arrays:
+                lattice_host = self._arrays[array_handle]
+            else:
+                lattice_host = f90wrap.runtime.get_array(f90wrap.runtime.sizeof_fortran_t,
+                                        self._handle,
+                                        _raffle.f90wrap_raffle_generator_type__array__lattice_host)
+                self._arrays[array_handle] = lattice_host
+            return lattice_host
+        
+        @lattice_host.setter
+        def lattice_host(self, lattice_host):
+            self.lattice_host[...] = lattice_host
+        
+        @property
+        def method_probab(self):
+            """
+            Element method_probab ftype=real(real12) pytype=float
+            
+            
+            Defined at ../src/lib/mod_generator.f90 line \
+                28
+            
+            """
+            array_ndim, array_type, array_shape, array_handle = \
+                _raffle.f90wrap_raffle_generator_type__array__method_probab(self._handle)
+            if array_handle in self._arrays:
+                method_probab = self._arrays[array_handle]
+            else:
+                method_probab = f90wrap.runtime.get_array(f90wrap.runtime.sizeof_fortran_t,
+                                        self._handle,
+                                        _raffle.f90wrap_raffle_generator_type__array__method_probab)
+                self._arrays[array_handle] = method_probab
+            return method_probab
+        
+        @method_probab.setter
+        def method_probab(self, method_probab):
+            self.method_probab[...] = method_probab
+        
+        def __str__(self):
+            ret = ['<raffle_generator_type>{\n']
+            ret.append('    bins : ')
+            ret.append(repr(self.bins))
+            ret.append(',\n    lattice_host : ')
+            ret.append(repr(self.lattice_host))
+            ret.append(',\n    method_probab : ')
+            ret.append(repr(self.method_probab))
+            ret.append('}')
+            return ''.join(ret)
+        
+        _dt_array_initialisers = []
+        
+    
+    _dt_array_initialisers = []
+    
+
+generator = Generator()
+
+class Raffle(f90wrap.runtime.FortranModule):
+    """
+    Module raffle
+    
+    
+    Defined at ../src/raffle.f90 lines 1-4
+    
+    """
+    pass
+    _dt_array_initialisers = []
+    
+
+raffle = Raffle()
+

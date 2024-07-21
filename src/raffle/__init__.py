d98f394a6da6c04e44ad9ad85f6701a5597ab70f
Ned Taylor <nedttaylor@gmail.com>
2024-07-21 09:32:44 +0100

    Set up pip install

---
+++ src/raffle/__init__.py
@@ -0,0 +1,12 @@
+"""
+raffle package
+
+This package provides functionality to interface with a Fortran library,
+including a Python wrapper around the Fortran code.
+"""
+
+__version__ = '0.2.0'
+
+from raffle.raffle import *
+
+__all__ = ['__version__', 'generator', 'rw_geom']
--- src/raffle/raffle.py
+++ src/raffle/raffle.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, absolute_import, division
-import _raffle
+import raffle._raffle
 import f90wrap.runtime
 import logging
 import numpy

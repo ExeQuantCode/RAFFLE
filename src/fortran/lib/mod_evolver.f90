5bc64c214d82ada60c5452c9f5fe8344fa8f84f6
Ned Taylor <nedttaylor@gmail.com>
2024-08-18 08:52:22 +0100

    Improve error printing

--- src/fortran/lib/mod_evolver.f90
+++ src/fortran/lib/mod_evolver.f90
@@ -1098,7 +1098,10 @@ module evolver
     idx2 = findloc([ element_database(:)%name ], &
          elements(2), dim=1)
    if(idx1.lt.1.or.idx2.lt.1)then
-      write(0,*) "ERROR", idx1, idx2
+      write(0,*) "ERROR: Element not found in database"
+      if(idx1.lt.1) write(0,*) "Element: ", elements(1)
+      if(idx2.lt.1) write(0,*) "Element: ", elements(2)
+      write(0,*) "Indices: ", idx1, idx2
       stop 1
    end if
     radius = ( element_database(idx1)%radius + &

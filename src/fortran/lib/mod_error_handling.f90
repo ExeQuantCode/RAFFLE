01ce153308d69718413cdb7f49bbbc51514bb555
Ned Taylor <nedttaylor@gmail.com>
2024-09-07 15:43:47 +0100

    Add uniform error handling

---
+++ src/fortran/lib/mod_error_handling.f90
@@ -0,0 +1,33 @@
+module error_handling
+  implicit none
+  logical :: test_error_handling = .false.
+  
+  private
+
+  public :: test_error_handling
+  public :: stop_program
+
+
+contains
+
+  module subroutine stop_program(message, exit_code)
+    !! Stop the program and print an error message.
+    implicit none
+    character(len=*), intent(in) :: message
+    integer, intent(in), optional :: exit_code
+
+    integer :: exit_code_
+
+    if(present(exit_code)) then
+       exit_code_ = exit_code
+    else
+       exit_code_ = 1
+    end if
+
+    write(0,*) 'ERROR: ', trim(message)
+    if(.not.test_error_handling)then
+       stop exit_code_
+    end if
+  end subroutine stop_program
+
+end module error_handling

a4e6ddb3536d930368ad4fb99dd289df17946ce0
Ned Taylor <nedttaylor@gmail.com>
2024-09-08 09:30:31 +0100

    Add unit tests

--- test/test_evaluator.f90
+++ test/test_evaluator.f90
@@ -1,6 +1,7 @@
 program test_evaluator
+  use error_handling
   use constants, only: real12
-  use rw_geom, only: basis_type, geom_write
+  use rw_geom, only: basis_type
   use extended_geom, only: extended_basis_type
   use evaluator, only: evaluate_point
   use generator, only: raffle_generator_type
@@ -9,13 +10,15 @@ program test_evaluator
 
 
   integer :: unit
-  integer :: i, j, k, num_points
+  integer :: i, ia, num_points
   integer :: best_loc
+  logical :: success = .true.
   type(extended_basis_type) :: basis_host
   type(basis_type), dimension(1) :: database
   character(3), dimension(1) :: element_symbols
   real(real12), dimension(1) :: element_energies
-  integer, dimension(1,2) :: atom_ignore_list
+  real(real12), dimension(3) :: tolerance
+  integer, dimension(:,:), allocatable :: atom_ignore_list
 
   real(real12), dimension(:), allocatable :: tmp_vals
   real(real12), dimension(:), allocatable :: suitability_grid
@@ -26,7 +29,8 @@ program test_evaluator
 
   type(raffle_generator_type) :: generator
 
-  
+  test_error_handling = .true.
+
   !-----------------------------------------------------------------------------
   ! set up database
   !-----------------------------------------------------------------------------
@@ -87,7 +91,7 @@ program test_evaluator
   basis_host%sysname = 'diamond'
   basis_host%nspec = 1
   allocate(basis_host%spec(basis_host%nspec))
-  basis_host%spec(1)%num = 11
+  basis_host%spec(1)%num = 16
   basis_host%spec(1)%name = 'C'
   basis_host%natom = sum(basis_host%spec(:)%num)
   allocate(basis_host%spec(1)%atom(basis_host%spec(1)%num, 3))
@@ -101,17 +105,24 @@ program test_evaluator
   basis_host%spec(1)%atom(8, :3) = [0.25, 0.75, 0.375]
   basis_host%spec(1)%atom(9, :3) = [0.0, 0.0, 0.5]
   basis_host%spec(1)%atom(10, :3) = [0.5, 0.5, 0.5]
-!   basis_host%spec(1)%atom(11, :3) = [0.722, 0.722, 0.6388]
-!   basis_host%spec(1)%atom(12, :3) = [0.222, 0.222, 0.6111]
-!   basis_host%spec(1)%atom(13, :3) = [-0.056, 0.5, 0.75]
-!   basis_host%spec(1)%atom(14, :3) = [0.5, -0.056, 0.75]
-  basis_host%spec(1)%atom(basis_host%spec(1)%num, :3) = [0.0, 0.0, 0.0]
-  atom_ignore_list(1,1) = 1
-  atom_ignore_list(1,2) = basis_host%spec(1)%num
-
+  basis_host%spec(1)%atom(11, :3) = [0.75, 0.25, 0.875]
+  basis_host%spec(1)%atom(12, :3) = [0.25, 0.75, 0.875]
+  basis_host%spec(1)%atom(13, :3) = [0.0, 0.5, 0.75]
+  basis_host%spec(1)%atom(14, :3) = [0.5, 0.0, 0.75]
+  basis_host%spec(1)%atom(15, :3) = [0.75, 0.75, 0.625]
+  basis_host%spec(1)%atom(16, :3) = [0.25, 0.25, 0.625]
   basis_host%lat(1,:) = [3.560745109, 0.0, 0.0]
   basis_host%lat(2,:) = [0.0, 3.560745109, 0.0]
   basis_host%lat(3,:) = [0.0, 0.0, 7.121490218]
+
+  ! set up atom_ignore_list
+  allocate(atom_ignore_list(8,2))
+  do i = 1, size(atom_ignore_list,1)
+     atom_ignore_list(i,1) = 1
+     atom_ignore_list(i,2) = &
+          basis_host%spec(1)%num - size(atom_ignore_list,1) + i
+  end do
+
 
 
 !   basis_host%sysname = 'graphite'
@@ -139,9 +150,6 @@ program test_evaluator
        max_bondlength = 6._real12, &
        atom_ignore_list = atom_ignore_list &
   )
-  open(newunit=unit, file="POSCAR_host_graphite", status="replace")
-  call geom_write(unit, basis_host)
-  close(unit)
 
 
   generator%distributions%kbt = 0.2
@@ -155,7 +163,7 @@ program test_evaluator
   !-----------------------------------------------------------------------------
   call generator%distributions%create( &
        basis_list = database, &
-       deallocate_systems = .false. &
+       deallocate_systems = .true. &
   )
 
 
@@ -163,15 +171,6 @@ program test_evaluator
   ! set up gridpoints
   !-----------------------------------------------------------------------------
   num_points = 0
-!   allocate(gridpoints(3, product(grid)))
-!   do i = 0, grid(1) - 1, 1
-!      do j = 0, grid(2) - 1, 1
-!         do k = 0, grid(3) - 1, 1
-!            num_points = num_points + 1
-!            gridpoints(:, num_points) = real([i + 0.5, j + 0.5, k + 0.5],real12) / real(grid, real12)
-!         end do
-!      end do
-!   end do
   gridpoints = get_viable_gridpoints( generator%grid, &
        basis_host, &
        [ generator%distributions%bond_info(:)%radius_covalent ], &
@@ -179,84 +178,78 @@ program test_evaluator
        lowtol = generator%distributions%radius_distance_tol(1), &
        grid_offset = generator%grid_offset &
   )
-  write(*,*) "Number of gridpoints: ", size(gridpoints, dim=2)
-
-
-  !-----------------------------------------------------------------------------
-  ! write distribution functions to file
-  !-----------------------------------------------------------------------------
-  call generator%distributions%write_2body(file="2body.txt")
-  call generator%distributions%write_3body(file="3body.txt")
-  call generator%distributions%write_4body(file="4body.txt")
-
-
-  !-----------------------------------------------------------------------------
-  ! check element energies
-  !-----------------------------------------------------------------------------
-  call generator%distributions%get_element_energies( &
-       tmp_symbols, &
-       tmp_vals &
-  )
-  do i = 1, size(tmp_symbols)
-     write(*,*) "Element: ", tmp_symbols(i), " energy: ", tmp_vals(i)
-  end do
-  call generator%distributions%get_bond_radii( &
-       tmp_pairs, &
-       tmp_vals &
-  )
-  do i = 1, size(tmp_symbols)
-     write(*,*) "Element1: ", tmp_pairs(i,1), "Element2: ", tmp_pairs(i,2), " radii: ", tmp_vals(i)
-  end do
-
 
 
   !-----------------------------------------------------------------------------
   ! call evaluator
   !-----------------------------------------------------------------------------
-  open(newunit=unit, file="evaluator.txt", status="replace")
-  do i = 1, basis_host%spec(1)%num - 1
-     write(unit, *) matmul(basis_host%spec(1)%atom(i, :), basis_host%lat)
-  end do
+!   open(newunit=unit, file="evaluator.txt", status="replace")
+!   do i = 1, basis_host%spec(1)%num
+!      if( any( atom_ignore_list(:,1) .eq. i ) )cycle
+!      write(unit, *) matmul(basis_host%spec(1)%atom(i, :), basis_host%lat)
+!   end do
 !   do i = 1, basis_host%image_spec(1)%num
 !      write(unit, *) matmul(basis_host%image_spec(1)%atom(i, :), basis_host%lat)
 !   end do
   write(unit, *)
 
-  write(*,*) "LOOK", basis_host%num_images
-  write(*,*) "Number of images: ", basis_host%image_spec(1)%num
-  allocate(suitability_grid(size(gridpoints,2)))
-  do i = 1, size(gridpoints,dim=2)
-     suitability_grid(i) = evaluate_point( generator%distributions, &
-          gridpoints(:,i), basis_host, &
-          atom_ignore_list, &
-          [ generator%distributions%bond_info(:)%radius_covalent ] &
-     )
-     write(unit, *) matmul(gridpoints(:,i),basis_host%lat), suitability_grid(i)
+
+  do i = 1, 3
+     tolerance(i) = 1._real12 / real(generator%grid(i),real12) / 2._real12
   end do
-  close(unit)
-  write(*,*) "Size of suitability grid: ", size(suitability_grid)
-  write(*,*) "Size of gridpoints: ", size(gridpoints,2)
-  write(*,*) "Max suitability: ", maxval(suitability_grid)
-  write(*,*) "Max location: ", maxloc(suitability_grid)
-  best_loc = maxloc(suitability_grid,dim=1)
-  write(*,*) gridpoints(:,best_loc)
-  basis_host%spec(1)%atom(basis_host%spec(1)%num, 1:3) = &
-       gridpoints(1:3,best_loc)
-  call basis_host%update_images( &
-       max_bondlength = generator%distributions%cutoff_max(1), &
-       is = atom_ignore_list(1,1), &
-       ia = atom_ignore_list(1,2) &
-  )
-  write(*,*) "Number of images after update: ", basis_host%image_spec(1)%num
+  allocate(suitability_grid(size(gridpoints,2)))
+  do ia = 1, size(atom_ignore_list,1)
+     do i = 1, size(gridpoints,dim=2)
+        suitability_grid(i) = evaluate_point( generator%distributions, &
+             gridpoints(:,i), basis_host, &
+             atom_ignore_list(ia:,:), &
+             [ generator%distributions%bond_info(:)%radius_covalent ] &
+        )
+     end do
+     ! close(unit)
+     best_loc = maxloc(suitability_grid,dim=1)
+     ! Check point is correct
+     call assert( &
+          all( &
+               abs( &
+                    gridpoints(:,best_loc) - &
+                    basis_host%spec(1)%atom(atom_ignore_list(ia,2),:3) &
+               ) .lt. tolerance + 1.E-6_real12 &
+          ), &
+          "Incorrect gridpoint found.", &
+          success &
+     )
+     call basis_host%update_images( &
+          max_bondlength = 6._real12, &
+          is = 1, ia = atom_ignore_list(ia,2) &
+     )
+  end do
 
-  ! !-----------------------------------------------------------------------------
-  ! ! generate random structures
-  ! !-----------------------------------------------------------------------------
-  ! write(*,*) "Generating structures"
-  ! call generator%generate( num_structures, &
-  !      stoich, &
-  !      method_probab )
-  ! write(*,*) "Structures have been successfully generated"
 
+  !-----------------------------------------------------------------------------
+  ! check for any failed tests
+  !-----------------------------------------------------------------------------
+  write(*,*) "----------------------------------------"
+  if(success)then
+     write(*,*) 'test_evaluator passed all tests'
+  else
+     write(0,*) 'test_evaluator failed one or more tests'
+     stop 1
+  end if
+
+contains
+
+!###############################################################################
+
+  subroutine assert(condition, message, success)
+     implicit none
+     logical, intent(in) :: condition
+     character(len=*), intent(in) :: message
+     logical, intent(inout) :: success
+     if (.not. condition) then
+       write(0,*) "Test failed: ", message
+       success = .false.
+     end if
+   end subroutine assert
 
 end program test_evaluator

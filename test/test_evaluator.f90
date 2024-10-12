e31e3ae84e861e3c463ede7e235ea3b2083c78c6
Ned Taylor <nedttaylor@gmail.com>
2024-10-12 10:14:23 +0100

    Fix missing modu

--- test/test_evaluator.f90
+++ test/test_evaluator.f90
@@ -1,6 +1,7 @@
 program test_evaluator
   use error_handling
   use constants, only: real12
+  use misc_linalg, only: modu
   use rw_geom, only: basis_type, geom_write
   use extended_geom, only: extended_basis_type
   use evaluator, only: evaluate_point

01bc853ba2dcb20beb4b39a7a3cd19b0361f425b
Ned Taylor <nedttaylor@gmail.com>
2024-12-16 11:49:22 +0000

    Change parallel process

--- example/python_pkg/C_learn/learn.py
+++ example/python_pkg/C_learn/learn.py
@@ -1,28 +1,58 @@
 from mace.calculators import mace_mp
 from raffle.generator import raffle_generator
 from ase import build, Atoms
-from ase.optimize import BFGS
+from ase.optimize import BFGS, FIRE
 from ase.io import write
 import numpy as np
 import os
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, wait, as_completed
+from multiprocessing import Process
+from copy import deepcopy
+from multiprocessing import Queue
+from joblib import Parallel, delayed
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def runInParallel(*fns):
+    proc = []
+    results = []
+    for fn in fns:
+        p = Process(target=fn)
+        p.start()
+        proc.append(p)
+    for p in proc:
+        results.append(p.join())
+
+    print("All processes finished")
+    print(results)
 
 
-def process_structure(i, atoms, num_structures_old, calc, optimise_structure):
+def process_structure_with_queue(i, structure, num_old, calc_params, optimise_structure, queue):
+    # Perform the computation
+    result = process_structure(i, structure, num_old, calc_params, optimise_structure)
+    queue.put(result)  # Report completion
+
+def process_structure(i, atoms, num_structures_old, calc_params, optimise_structure):
     if i < num_structures_old:
         return
     
     inew = i - num_structures_old
     atoms.calc = calc
-    positions_initial = atoms.get_positions()
+    # positions_initial = atoms.get_positions()
 
     # Calculate and save the initial energy per atom
     energy_unrlxd = atoms.get_potential_energy() / len(atoms)
+    print(f"Initial energy per atom: {energy_unrlxd}")
 
     # Optimise the structure if requested
     if optimise_structure:
-        optimizer = BFGS(atoms, trajectory = f"traje{inew}.traj", logfile=f"optimisation{inew}.log")
-        optimizer.run(fmax=0.05, steps=500)
+        optimizer = FIRE(atoms, trajectory = f"traje{inew}.traj", logfile=f"optimisation{inew}.log")
+        try:
+            optimizer.run(fmax=0.05, steps=500)
+        except Exception as e:
+            print(f"Optimisation failed: {e}")
+            return None, None, None
     
     # Save the optimised structure and its energy per atom
     energy_rlxd = atoms.get_potential_energy() / len(atoms)
@@ -55,12 +85,18 @@ def process_structure(i, atoms, num_structures_old, calc, optimise_structure):
 
 if __name__ == "__main__":
 
+    calc_params = {
+        "model": "medium",
+        "dispersion": False,
+        "default_dtype": "float32",
+        "device": 'cpu'
+    }
     calc = mace_mp(model="medium", dispersion=False, default_dtype="float32", device='cpu')
 
     crystal_structures = [
         'orthorhombic', 'diamond',
-        'bct', 'sc',
-        'fcc', 'bcc', 'hcp',
+        # 'bct', 'sc',
+        # 'fcc', 'bcc', 'hcp',
     ]
 
     hosts = []
@@ -89,14 +125,15 @@ if __name__ == "__main__":
         }
     )
     # set energy scale
-    generator.distributions.set_kBT(0.4)
+    generator.distributions.set_kBT(0.2)
     # set the distribution function widths (2-body, 3-body, 4-body)
     generator.distributions.set_width([0.025, np.pi/200.0, np.pi/200.0])
 
     initial_database = [Atoms('C', positions=[(0, 0, 0)], cell=[5, 5, 5])]
     initial_database[0].calc = calc
 
-    generator.distributions.create(initial_database)
+    # generator.distributions.create(initial_database)
+    generator.distributions.read_gdfs("DATTEMPT4/gdfs.txt")
 
     if os.path.exists("energies_rlxd.txt"):
         with open("energies_rlxd.txt", "w") as energy_file:
@@ -117,87 +154,90 @@ if __name__ == "__main__":
         print("setting host")
         generator.set_host(host)
         volume = host.get_volume()
-        for num_atoms in range(1, 20):
+        for num_atoms in range(10, 80):
 
             density = ( num_atoms + 1 ) * mass / volume
             if density > 2.4: # aluminium density is 2.7 g/cm^3 ~= 1.61 u/A^3
-                print("Density too high:", density, "u/A^3")
+                # print("Density too high:", density, "u/A^3")
                 continue
-            elif density < 1.0:
-                print("Density too low", density, "u/A^3")
+            elif density < 2.0:
+                # print("Density too low", density, "u/A^3")
                 continue
 
-            seed += 1
-            print(f"Seed: {seed}")
-            generator.generate(
-                num_structures = 5,
-                stoichiometry = { 'C': num_atoms },
-                seed = seed,
-                method_probab = {"void": 1.0, "rand": 1.0, "walk": 1.0, "grow": 0.0, "min": 1.0},
-                verbose = 0,
-            )
+            for i in range(2):
+                seed += 1
+                print(f"Seed: {seed}")
+                generator.generate(
+                    num_structures = 5,
+                    stoichiometry = { 'C': num_atoms },
+                    seed = seed,
+                    method_probab = {"void": 0.5, "rand": 0.0, "walk": 0.5, "grow": 0.0, "min": 1.0},
+                    verbose = 0,
+                )
 
-            # print the number of structures generated
-            print("Total number of structures generated: ", generator.num_structures)
-            generated_structures = generator.get_structures(calc)
-            num_structures_new = len(generated_structures)
+                # print the number of structures generated
+                print("Total number of structures generated: ", generator.num_structures)
+                generated_structures = generator.get_structures(calc)
+                num_structures_new = len(generated_structures)
 
-            # check if directory iteration[iter] exists, if not create it
-            iterdir = f"iteration{seed}/"
-            if not os.path.exists(iterdir):
-                os.makedirs(iterdir)
+                # check if directory iteration[iter] exists, if not create it
+                iterdir = f"iteration{seed}/"
+                if not os.path.exists(iterdir):
+                    os.makedirs(iterdir)
+                generator.print_settings(iterdir+"generator_settings.txt")
 
-            # set up list of energies
-            energy_unrlxd = np.zeros(num_structures_new - num_structures_old)
-            energy_rlxd = np.zeros(num_structures_new - num_structures_old)
-            for i in range(num_structures_new - num_structures_old):
-                write(iterdir+f"POSCAR_unrlxd_{i}", generated_structures[num_structures_old + i])
-                print(f"Structure {i} energy per atom: {generated_structures[num_structures_old + i].get_potential_energy() / len(generated_structures[num_structures_old + i])}")
+                # set up list of energies
+                energy_unrlxd = np.zeros(num_structures_new - num_structures_old)
+                energy_rlxd = np.zeros(num_structures_new - num_structures_old)
+                for i in range(num_structures_new - num_structures_old):
+                    write(iterdir+f"POSCAR_unrlxd_{i}", generated_structures[num_structures_old + i])
+                    print(f"Structure {i} energy per atom: {generated_structures[num_structures_old + i].get_potential_energy() / len(generated_structures[num_structures_old + i])}")
 
-            # Parallel execution
-            with ProcessPoolExecutor() as executor:
-                futures = [
-                    executor.submit(process_structure, i, generated_structures[i], num_structures_old, calc, optimise_structure)
+                # Start parallel execution
+                print("Starting parallel execution")
+                results = Parallel(n_jobs=5)(
+                    delayed(process_structure)(i, deepcopy(generated_structures[i]), num_structures_old, calc_params, optimise_structure)
                     for i in range(num_structures_old, num_structures_new)
-                ]
+                )
 
                 # Wait for all futures to complete
-                for j, future in enumerate(futures):
-                    generated_structures[j+num_structures_old], energy_unrlxd[j], energy_rlxd[j] = future.result()
+                for j, result in enumerate(results):
+                    generated_structures[j+num_structures_old], energy_unrlxd[j], energy_rlxd[j] = result
+                print("All futures completed")
 
-            # Remove structures that failed the checks
-            for j, atoms in reversed(list(enumerate(generated_structures))):
-                if atoms is None:
-                    energy_unrlxd = np.delete(energy_unrlxd, j-num_structures_old)
-                    energy_rlxd = np.delete(energy_rlxd, j-num_structures_old)
-                    del generated_structures[j]
-                    generator.remove_structure(j)
-            num_structures_new = len(generated_structures) 
+                # Remove structures that failed the checks
+                for j, atoms in reversed(list(enumerate(generated_structures))):
+                    if atoms is None:
+                        energy_unrlxd = np.delete(energy_unrlxd, j-num_structures_old)
+                        energy_rlxd = np.delete(energy_rlxd, j-num_structures_old)
+                        del generated_structures[j]
+                        generator.remove_structure(j)
+                num_structures_new = len(generated_structures) 
 
-            # write the structures to files
-            for i in range(num_structures_new - num_structures_old):
-                write(iterdir+f"POSCAR_{i}", generated_structures[num_structures_old + i])
-                print(f"Structure {i} energy per atom: {energy_rlxd[i]}")
-                # append energy per atom to the energies_unrlxd.txt file
-                with open("energies_unrlxd.txt", "a") as energy_file:
-                    energy_file.write(f"{energy_unrlxd[i]}\n")
-                # append energy per atom to the energies_rlxd.txt file
-                with open("energies_rlxd.txt", "a") as energy_file:
-                    energy_file.write(f"{energy_rlxd[i]}\n")
+                # write the structures to files
+                for i in range(num_structures_new - num_structures_old):
+                    write(iterdir+f"POSCAR_{i}", generated_structures[num_structures_old + i])
+                    print(f"Structure {i} energy per atom: {energy_rlxd[i]}")
+                    # append energy per atom to the energies_unrlxd.txt file
+                    with open("energies_unrlxd.txt", "a") as energy_file:
+                        energy_file.write(f"{i+num_structures_old} {energy_unrlxd[i]}\n")
+                    # append energy per atom to the energies_rlxd.txt file
+                    with open("energies_rlxd.txt", "a") as energy_file:
+                        energy_file.write(f"{i+num_structures_old} {energy_rlxd[i]}\n")
 
-            # update the distribution functions
-            print("Updating distributions")
-            generator.distributions.update(generated_structures[num_structures_old:], from_host=False, deallocate_systems=False)
+                # update the distribution functions
+                print("Updating distributions")
+                generator.distributions.update(generated_structures[num_structures_old:], from_host=False, deallocate_systems=False)
 
-            # print the new distribution functions to a file
-            print("Printing distributions")
-            generator.distributions.write_dfs(iterdir+"distributions.txt")
-            generator.distributions.write_2body(iterdir+"df2.txt")
-            generator.distributions.write_3body(iterdir+"df3.txt")
-            generator.distributions.write_4body(iterdir+"df4.txt")
+                # print the new distribution functions to a file
+                print("Printing distributions")
+                generator.distributions.write_dfs(iterdir+"distributions.txt")
+                generator.distributions.write_2body(iterdir+"df2.txt")
+                generator.distributions.write_3body(iterdir+"df3.txt")
+                generator.distributions.write_4body(iterdir+"df4.txt")
 
-            # update the number of structures generated
-            num_structures_old = num_structures_new
+                # update the number of structures generated
+                num_structures_old = num_structures_new
 
     generator.distributions.write_gdfs("gdfs.txt")
 
@@ -207,9 +247,9 @@ if __name__ == "__main__":
 
     # Parse and sort the energies
     energies = [line.strip().split() for line in energies]
-    energies = sorted(energies, key=lambda x: float(x[0]))
+    energies = sorted(energies, key=lambda x: float(x[1]))
 
     # Write the sorted energies back to the file
     with open("energies_ordered.txt", "w") as energy_file:
         for entry in energies:
-            energy_file.write(f"{entry[0]}\n")
+            energy_file.write(f"{int(entry[0])} {float(entry[1])}\n")
